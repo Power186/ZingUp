ec092ddf9db0a0e16a1785cd42a6e1a0a9700d98
Scott <agentwinburn1@gmail.com>
2020-12-16 14:37:09 +0800

    Initial commit.

---
+++ ZingUp/ZingUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZingUpApp.swift
+//  ZingUp
+//
+//  Created by Scott on 12/16/20.
+//
+
+import SwiftUI
+
+@main
+struct ZingUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
